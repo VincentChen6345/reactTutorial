cadc41b9d7c6934dea81d764c269f8f51cf336d1
VincentChen6345 <vincent.chen6345@hotmail.com>
2022-07-04 12:14:30 +1000

    'dynamic list rendering '

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,26 +1,16 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
-import React, { useState } from "react";
+import React from "react";
 
 function ExpenseItem(props) {
-  const [title, setTitle] = useState(props.title);
-
-  let amount = props.amount;
-  let date = props.date;
-  const clickHandler = () => {
-    setTitle("Hi!");
-    console.log(title);
-  };
-
   return (
     <Card className="expense-item">
-      <ExpenseDate date={date}></ExpenseDate>
+      <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">${amount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
